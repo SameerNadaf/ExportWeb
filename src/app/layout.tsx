e52dd90a14f9ed3ba7971c68ab8c2e83924a6c8a
Sameer Nadaf <nsameernadaf@gmail.com>
2026-01-01 23:54:35 +0530

    feat: integrate Google Analytics using @next/third-parties

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { OrganizationSchema } from "@/components/seo/JsonLd";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -83,6 +84,7 @@ export default function RootLayout({
           {children}
           <OrganizationSchema />
         </ThemeProvider>
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID || ""} />
       </body>
     </html>
   );
