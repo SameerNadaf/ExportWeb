25e5233b8e333f0ee9dccc06498ac81fa8cf2bae
Sameer Nadaf <nsameernadaf@gmail.com>
2026-01-02 23:03:57 +0530

    feat: Integrate next-pwa for Progressive Web App capabilities, adding service worker files and updating build configuration.

--- next.config.ts
+++ next.config.ts
@@ -48,4 +48,11 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+export default withPWA(nextConfig);
